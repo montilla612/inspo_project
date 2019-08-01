c5bf7a73a343525b4b55175d3fcae4234cd2e8a6
EC2 Default User <ec2-user@ip-172-31-23-125.us-east-2.compute.internal>
2019-08-01 14:28:30 +0000

    initial commit

--- app/models/model.py
+++ app/models/model.py
@@ -1,75 +1,219 @@
 import random
 
-ambitious = [ "Shoutout to the girls who put in major work. Who are up early ass hell for school/work. Girls that study. Grind. Who persevere & prioritize self worth/lov. Who rise above & hold it down even when they're falling apart, to the girls whopush harder.", "I used to think going to sleep late was cool. Till I realized wakin up early was the real boss shit.", "if you look at people in your circle and don't get inspired.then you don't have a circle, you have a cage", "shout out to everyone making progress that no one recognize", "Don't limit your challlenges. Challenge your limits!", "Yes it is going to be hard but it's going to be worth it!"
+Ambitious = [ "Shoutout to the girls who put in major work. Who are up early ass hell for school/work. Girls that study. Grind. Who persevere & prioritize self worth/lov. Who rise above & hold it down even when they're falling apart, to the girls whopush harder.", "I used to think going to sleep late was cool. Till I realized wakin up early was the real boss shit.", "if you look at people in your circle and don't get inspired.then you don't have a circle, you have a cage", "shout out to everyone making progress that no one recognize", "Don't limit your challlenges. Challenge your limits!", "Yes it is going to be hard but it's going to be worth it!"
     ]
-angry = [ "Speak when you are angry and you'll make the best speech you'll ever REGRET!!!", "Never let your emotions overpower your intelligence!!!", "Don't do something permanentely stupid just because you're temporarily upset!!", "Anger dosen't solve anything. It builds nothing, but it can destroy everything.", "You will not be punished for your anger, you will be punished by your anger."]
+Angry = [ "Speak when you are angry and you'll make the best speech you'll ever REGRET!!!", "Never let your emotions overpower your intelligence!!!", "Don't do something permanentely stupid just because you're temporarily upset!!", "Anger dosen't solve anything. It builds nothing, but it can destroy everything.", "You will not be punished for your anger, you will be punished by your anger."]
 
-sad = [ "Be strong because things will get better. It maybe stormy now but it never rains forever! ;)", "You are not allowed to let sadness ruin you. ;)", "It's not about forcing sadness it's about not leting sadness win. ;)", "Breathe it's only a bad day not a bad life. ;)", "I was sad but then I went beast mode! ;)", ]
+Sad = [ "Be strong because things will get better. It maybe stormy now but it never rains forever! ;)", "You are not allowed to let sadness ruin you. ;)", "It's not about forcing sadness it's about not leting sadness win. ;)", "Breathe it's only a bad day not a bad life. ;)", "I was sad but then I went beast mode! ;)", ]
 
-happy = [ "Being happy NEVER goes out of style. :)", "Happiness is letting go of what you think your life is supposed to be. :)", "It's fun to be happy. :)", "People look at you as a role model when you happy","Happiness never decreases by being shared" ]
+Happy = [ "Being happy NEVER goes out of style. :)", "Happiness is letting go of what you think your life is supposed to be. :)", "It's fun to be happy. :)", "People look at you as a role model when you happy. :)","Happiness never decreases by being shared. :)" ]
 
-woke = [ "It's not the world that's cruel. It's the people in it.", "Life is not a problem to be solved, but a reality to be experienced.", "Dead people receive more flowers than the living ones because regret is stronger than gratitude.", ""
+Woke = [ "It's not the world that's cruel. It's the people in it.", "Life is not a problem to be solved, but a reality to be experienced.", "Dead people receive more flowers than the living ones because regret is stronger than gratitude.", "Listen to your possibilites not your insecurities", "OUTSIDERS know all your business from INSIDERS. ", "Be forgiving, Be understanding, But don't be a fool.", 
     
     
     ]
 
 
-random.shuffle(ambitious)
-random.shuffle(sad)
-random.shuffle(angry)
-random.shuffle(happy)
+random.shuffle(Ambitious)
+random.shuffle(Sad)
+random.shuffle(Angry)
+random.shuffle(Happy)
+random.shuffle(Woke)
 
 
-def find_quote(mood):  
-    if mood == "Ambitious":
-        random.shuffle(ambitious)
-        one_quote = ambitious[0]
+# def submit_emotion(mood):
+#     if mood == "Happy":
+#         return "Happy quote"
+    
+
+
+def find_quote(mood): 
+    if mood.capitalize() == "Ambitious":
+        random.shuffle(Ambitious)
+        one_quote = Ambitious[0]
         return one_quote
-    elif mood == "Sad":
-        random.shuffle(sad)
-        one_quote = sad[0]
+    elif mood.capitalize() == "Sad":
+        random.shuffle(Sad)
+        one_quote = Sad[0]
         return one_quote
-    elif mood == "Happy":
-        random.shuffle(happy)
-        one_quote = happy[0]
+    elif mood.capitalize() == "Happy":
+        random.shuffle(Happy)
+        one_quote = Happy[0]
         return one_quote
-    elif mood == "Angry":
-        random.shuffle(angry)
-        one_quote = angry[0]
+    elif mood.capitalize() == "Angry":
+        random.shuffle(Angry)
+        one_quote = Angry[0]
         return one_quote
+    elif mood.capitalize() == "Woke":
+        random.shuffle(Woke)
+        one_quote = Woke[0]
+        return one_quote
+        
+        
     else:
         return "Sorry Try Again!"
     return find_quote(mood)
-print (find_quote("happy"))
-def find_gif(mood):
-    if mood == "Ambitious":
-      return "/static/ambitious-picture.gif"
-    elif mood == "Sad":
-        return "/static/sad-picture.gif"
-    elif mood == "sad":
-         return "/static/sad-picture.gif"
-    elif mood == "Happy":
-        return "/static/happy-picture.gif"
-    elif mood == "Angry":
-        return "/static/angry-picture.gif"
-    else:
-        return "Please type in a real emotion"
-    return find_quote(mood)
 
-def find_image(mood):
-    if mood == "Ambitious":
-      return "/static/ambitious-photo.jpg"
-    elif mood == "Sad":
-        return "/static/sad-photo.jpg"
-    elif mood == "sad":
-         return "/static/sad-photo.jpg"
-    elif mood == "Happy":
-        return "/static/happy-photo.jpg"
-    elif mood == "Angry":
-        return "/static/angry-photo.jpg"
-    else:
-        return "Please type in a real emotion"
-    return find_image(mood)
+
+
+    
+    
+    
+# def find_gif(mood):
+#     if mood == "Ambitious":
+#       return "/static/ambitious-picture.gif"
+#     elif mood == "Sad":
+#         return "/static/sad-picture.gif"
+#     elif mood == "sad":
+#          return "/static/sad-picture.gif"
+#     elif mood == "Happy":
+#         return "/static/happy-picture.gif"
+#     elif mood == "Angry":
+#         return "/static/angry-picture.gif"
+#     else:
+#         return "Please type in a real emotion"
+#     return find_quote(mood)
+
+# def find_image(mood):
+#     if mood == "Ambitious":
+#       return "/static/ambitious-photo.jpg"
+#     elif mood == "Sad":
+#         return "/static/sad-photo.jpg"
+#     elif mood == "sad":
+#          return "/static/sad-photo.jpg"
+#     elif mood == "Happy":
+#         return "/static/happy-photo.jpg"
+#     elif mood == "Angry":
+#         return "/static/angry-photo.jpg"
+#     else:
+#         return "Please type in a real emotion"
+#     return find_image(mood)
     
 # print(ambitious[0])   
+
+def find_pointsq1(answer):
+    if answer == "q1-a1":
+        pointcounter = 0
+        return pointcounter
+    if answer == "q1-a2":
+        pointcounter = 1
+        return pointcounter
+    else:
+        pointcounter = 2
+        return pointcounter
+
+def find_pointsq2(answer):
+    if answer == "q2-a1":
+        pointcounter = 0
+        return pointcounter
+    if answer == "q2-a2":
+        pointcounter = 1
+        return pointcounter
+    else:
+        pointcounter = 2
+        return pointcounter
+        
+def find_pointsq3(answer):
+    if answer == "q3-a1":
+        pointcounter = 2
+        return pointcounter
+    if answer == "q3-a2":
+        pointcounter = 1
+        return pointcounter
+    else:
+        pointcounter = 0
+        return pointcounter
+
+def find_pointsq4(answer):
+    if answer == "q4-a1":
+        pointcounter = 2
+        return pointcounter
+    if answer == "q4-a2":
+        pointcounter = 1
+        return pointcounter
+    else:
+        pointcounter = 0
+        return pointcounter
+ 
+def find_pointsq5(answer):
+    if answer == "q5-a1":
+        pointcounter = 2
+        return pointcounter
+    if answer == "q5-a2":
+        pointcounter = 0
+        return pointcounter
+    else:
+        pointcounter = 1
+        return pointcounter  
+ 
+def find_pointsq6(answer):
+    if answer == "q6-a1":
+        pointcounter = 2
+        return pointcounter
+    if answer == "q6-a2":
+        pointcounter = 0
+        return pointcounter
+    else:
+        pointcounter = 1
+        return pointcounter   
+
+def find_pointsq7(answer):
+    if answer == "q7-a1":
+        pointcounter = 0
+        return pointcounter
+    if answer == "q7-a2":
+        pointcounter = 2
+        return pointcounter
+    else:
+        pointcounter = 1
+        return pointcounter   
+  
+def find_pointsq8(answer):
+    if answer == "q8-a1":
+        pointcounter = 0
+        return pointcounter
+    if answer == "q8-a2":
+        pointcounter = 1
+        return pointcounter
+    else:
+        pointcounter = 2
+        return pointcounter  
+
+def find_pointsq9(answer):
+    if answer == "q9-a1":
+        pointcounter = 0
+        return pointcounter
+    if answer == "q9-a2":
+        pointcounter = 1
+        return pointcounter
+    else:
+        pointcounter = 2
+        return pointcounter  
+
+def find_pointsq10(answer):
+    if answer == "q10-a1":
+        pointcounter = 0
+        return pointcounter
+    if answer == "q10-a2":
+        pointcounter = 1
+        return pointcounter
+    else:
+        pointcounter = 2
+        return pointcounter  
+
+from app import app
+from flask import render_template, request
+from app.models import model, formopener
+
+def depression_suggestion(total_points):
+    if total_points >= 15: 
+        return ("You may be depressed")
+      
+    elif total_points > 8 and total_points <= 14:
+        return("You are doing ok")
+    else:
+        return("You are very happy")
+    return 
+
+
 
--- app/routes.py
+++ app/routes.py
@@ -14,42 +14,101 @@ def results():
     if request.method == "GET":
         return render_template("index.html")
     else:
-        output = request.form['mood']
-        print(output)
-        emotion_output = model.find_quote(output)
-        return render_template("results.html", output = output, emotion_output = emotion_output)
+        user_data = request.form
+        print(user_data)
+        user_mood = user_data ["mood"]
+        print(user_mood)
+        mood_quote = model.find_quote(user_mood)
+        print(mood_quote)
+        return render_template("results.html", user_mood = user_mood, mood_quote = mood_quote) 
+
+@app.route('/quiz', methods = ["GET", "POST"])
+def quiz():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        return render_template("quiz.html")
+        
+@app.route('/quizresults', methods = ["GET", "POST"])
+def quizresults():
+    if request.method == "GET":
+        return render_template("quiz.html")
+    else:
+        user_data = request.form
+        print(user_data)
+        user_answer1 = user_data("q1")
+        print(user_answer1)
+        user_answer2 = user_data("q2")
+        
+        user_answer3 = user_data("q3")
+        
+        user_answer4 = user_data("q4")
+        
+        user_answer5 = user_data("q5")
+        
+        user_answer6 = user_data("q6")
+        
+        user_answer7 = user_data("q7")
+        
+        user_answer8 = user_data("q8")
+        
+        user_answer9 = user_data("q9")
+        
+        user_answer10 = user_data("q10")
+        
+        total_points = model.find_pointsq1(user_answer1) + model.find_pointsq2(user_answer2) + model.find_pointsq3(user_answer3) + model.find_pointsq4(user_answer4) + model.find_pointsq5(user_answer5) + model.find_pointsq6(user_answer6) + model.find_pointsq7(user_answer7) + model.find_pointsq8(user_answer8) + model.find_pointsq9(user_answer9) + model.find_pointsq10(user_answer10) 
+        if total_points > 15:
+            return("You are depressed")
+        elif total_points > 8 and total_points <= 14:
+            return("You are ok")
+        else:
+            return("You are happy")
+            
+        return render_template("quizresults.html", user_answer1 = user_answer1, user_answer2 = user_answer2, user_answer3 = user_answer3, user_answer4 = user_answer4, user_answer5 = user_answer5, user_answer6 = user_answer6, user_answer7 = user_answer7, user_answer8 = user_answer8, user_answer9 = user_answer9, user_answer10 = user_answer10, total_points = total_points)
+        
+       
+               
+        
+        
+        
+        
+        
+        
+       
 
 
-@app.route('/happy', methods = ["GET", "POST"])
-def happy_result():
-    if request.method == "GET":
-        quote = model.find_quote("Happy")
-        gif = model.find_gif("happy")
-        image = model.find_image("happy")
-        return render_template("results.html", quote = quote, gif = gif, image = image)
+# @app.route('/happy', methods = ["GET", "POST"])
+# def happy_result():
+#     if request.method == "GET":
+#         # user_data = request.form
+#         # user_mood = ...
+#         quote = model.find_quote("Happy")
+#         gif = model.find_gif("Happy")
+#         image = model.find_image("Happy")
+#         return render_template("results.html", quote = quote, gif = gif, image = image)
 
-@app.route('/sad', methods = ["GET", "POST"])
-def sad_result():
-    if request.method == "GET":
-        quote = model.find_quote("Sad")
-        gif = model.find_gif("sad")
-        image = model.find_image("sad")
-        return render_template("results.html", quote = quote, gif = gif, image = image)
-@app.route('/angry', methods = ["GET", "POST"])
-def angry_result():
-    if request.method == "GET":
-        quote = model.find_quote("Angry")
-        gif = model.find_gif("angry")
-        image = model.find_image("angry")
-        return render_template("results.html", quote = quote, gif = gif, image = image)
+# @app.route('/sad', methods = ["GET", "POST"])
+# def sad_result():
+#     if request.method == "GET":
+#         quote = model.find_quote("Sad")
+#         gif = model.find_gif("Sad")
+#         image = model.find_image("Sad")
+#         return render_template("results.html", quote = quote, gif = gif, image = image)
+# @app.route('/angry', methods = ["GET", "POST"])
+# def angry_result():
+#     if request.method == "GET":
+#         quote = model.find_quote("Angry")
+#         gif = model.find_gif("Angry")
+#         image = model.find_image("Angry")
+#         return render_template("results.html", quote = quote, gif = gif, image = image)
 
-@app.route('/ambitious', methods = ["GET", "POST"])
-def ambitious_result():
-    if request.method == "GET":
-        quote = model.find_quote("Ambitious")
-        gif = model.find_gif("ambitious")
-        image = model.find_image("ambitious")
-        return render_template("results.html", quote = quote, gif = gif, image = image)
+# @app.route('/ambitious', methods = ["GET", "POST"])
+# def ambitious_result():
+#     if request.method == "GET":
+#         quote = model.find_quote("Ambitious")
+#         gif = model.find_gif("Ambitious")
+#         image = model.find_image("Ambitious")
+#         return render_template("results.html", quote = quote, gif = gif, image = image)
 
    
     # else:
